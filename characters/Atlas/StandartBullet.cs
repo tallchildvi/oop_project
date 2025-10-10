34d297f1fb30b9cf3491f21666000c052a07a788
RostyslavBoichuk <rostykb2405@gmail.com>
2025-10-10 04:47:06 +0300

    Add files via upload

--- characters/Atlas/StandartBullet.cs
+++ characters/Atlas/StandartBullet.cs
@@ -1,24 +1,12 @@
 using Godot;
-using System;
 
-public partial class StandartBullet : Sprite2D
+public partial class StandartBullet : BulletInheritance
 {
-	private Vector2 movement;
-	private float speed = 400f;
-	
-	public void Init(Vector2 direction, bool facingRight)
+	public override void _Ready()
 	{
-		movement = direction.Normalized();
-		if (movement == Vector2.Zero){
-			if(facingRight) movement.X += 1;
-			else movement.X -= 1;
-		}
-	}
-	
-	public override void _Process(double delta)
-	{
-		Position += movement * speed * (float)delta;
-		float angle = Mathf.Atan2(movement.Y, movement.X);
-		RotationDegrees = Mathf.RadToDeg(angle);
+		selfGroup = "player_bullet";
+		oppositeGroup = "enemy_bullet";
+		speed = 400f;
+		base._Ready();
 	}
 }
